3b452a120161c57e8287b75a5427a85857f861a1
omershehu-creator <elonashahini@umsh.edu.al>
2025-02-24 13:34:55 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    if (false) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (false) {
+    	
+    }
+})
+function showEmoji () {
+    basic.clearScreen()
+    if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    }
+}
+basic.showString("Rate me!")
